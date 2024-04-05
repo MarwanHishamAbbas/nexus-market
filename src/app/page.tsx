5ec1901c1e6c28d301eece5e679006201d78e92f
Marwan Hisham <marwanhiisham@outlook.com>
2024-04-05 21:43:37 +0200

    style(globals.css): modify somestyles
    adding important button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
+    <div className="mt-10">
       <Button>Click me</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"link"}>link</Button>
     </div>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,12 +12,11 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-input outline-btn",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-primary hover:text-primary/70",
       },
       size: {
         default: "h-9 px-4 py-2",
