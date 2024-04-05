9312db0c49b0585cb015eab2c5bd95a85c304127
Marwan Hisham <marwanhiisham@outlook.com>
2024-04-05 07:18:10 +0200

    refactor(taiwlind.config.ts): remove nextui
    Removed NextUI caues I hated it so much, but I kept Framer motion thought

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { Button } from "@nextui-org/button"
 import Image from "next/image"
 
 export default function Home() {
-  return (
-    <div>
-      <Button size="sm" color="primary">
-        Hello
-      </Button>
-      <Button size="sm" variant="flat">
-        Hello
-      </Button>
-    </div>
-  )
+  return <div></div>
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { nextui } from "@nextui-org/react"
 import type { Config } from "tailwindcss"
 
 const config: Config = {
@@ -6,7 +5,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
@@ -14,29 +12,5 @@ const config: Config = {
       white: "#F0F0F0",
     },
   },
-  darkMode: "class",
-  plugins: [
-    nextui({
-      themes: {
-        dark: {
-          colors: {
-            background: "#0A0A0A", // or DEFAULT
-            foreground: "#F0F0F0", // or 50 to 900 DEFAULT
-            primary: {
-              foreground: "#0A0A0A",
-              DEFAULT: "#F0F0F0",
-            },
-            secondary: {
-              foreground: "#F0F0F0",
-              DEFAULT: "#0A0A0A",
-            },
-          },
-          layout: {
-            borderWidth: { small: "1px" },
-          },
-        },
-      },
-    }),
-  ],
 }
 export default config
